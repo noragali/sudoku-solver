a76a5608b2f4b0d5be67e008d5fc091e17a1b21c
Nora <noragalina167@gmail.com>
2026-07-07 13:28:10 +0300

    Sudoku board made

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "Sudoku.h"
+using namespace std;
 
+void Sudoku::printBoard()
+{
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            if (board[row][col] == 0)
+                cout << ". ";
+            else
+                cout << board[row][col] << " ";
+
+            if (col == 2 || col == 5)
+                cout << "| ";
+        }
+
+        cout << endl;
+
+        if (row == 2 || row == 5)
+            cout << "---------------------" << endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include "Sudoku.h"
+using namespace std;
 
+int main() {
+    Sudoku sudoku;
+    sudoku.printBoard();
+    return 1;
+}
